ba7ac769530f55b26403564edbb8534d979430b7
Matthew Wong <fromosia@gmail.com>
2018-08-20 17:46:09 +0800

    Add package init

---
+++ FilterBanks/__init__.py
@@ -0,0 +1,5 @@
+from FilterBanks import Downsample, Upsample
+from TwoBandFilters import TwoBandDownsample, TwoBandUpsample
+from PresetFilters import DirectionalDicimator, DirectionalInterpolation, ParalleloidUpsampler, ParalleloidDecimator
+
+__all__ = ['Downsample', 'Upsample', 'TwoBandUpsample', 'TwoBandDownsample', 'DirectionalInterpolation', 'DirectionalDicimator', 'ParalleloidDecimator', 'ParalleloidUpsampler']
---
+++ _init_.py
@@ -0,0 +1,3 @@
+from FilterBanks import *
+
+__all__ = ['Downsample', 'Upsample', 'TwoBandUpsample', 'TwoBandDownsample', 'DirectionalInterpolation', 'DirectionalDicimator', 'ParalleloidDecimator', 'ParalleloidUpsampler']
