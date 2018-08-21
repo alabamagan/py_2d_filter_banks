8d56faef2d3ce40bf903919cb503a2fcee9ad096
Matthew Wong <fromosia@gmail.com>
2018-08-21 17:12:53 +0800

    Add utility functions

---
+++ Demo/Utility.py
@@ -0,0 +1,32 @@
+from numpy.fft import fftshift, ifft2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def display_subbands(image, ncol=2, display_freq=False, cmap='Greys_r'):
+    ncol = int(ncol)
+    assert ncol > 0
+    assert isinstance(image, np.ndarray)
+
+    if image.ndim == 3:
+        M = image.shape[-1]
+        fig, axs = plt.subplots(M / ncol, ncol)
+        for i in xrange(M):
+            if M <= ncol:
+                try:
+                    if display_freq:
+                        axs[i].imshow(np.abs(fftshift(image[:,:,i])), vmin=0, vmax=2500, cmap=cmap)
+                    else:
+                        axs[i].imshow(ifft2(fftshift(image[:,:,i])).real, cmap=cmap)
+                    axs[i].set_title('%s'%i)
+                except:
+                    pass
+            else:
+                if display_freq:
+                    axs[i//ncol, i%ncol].imshow(np.abs(fftshift(image[:,:,i])), vmin=0, vmax=2500, cmap=cmap)
+                else:
+                    axs[i//ncol, i%ncol].imshow(ifft2(fftshift(image[:,:,i])).real, cmap=cmap)
+                axs[i//ncol, i%ncol].set_title('%s'%i)
+        plt.show()
+    elif image.ndim == 2:
+        plt.imshow(image, cmap=cmap)
+        plt.show()
