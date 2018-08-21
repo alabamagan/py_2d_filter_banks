ce459a15e1456f64df8d49742b8430ad7c060638
Matthew Wong <fromosia@gmail.com>
2018-08-21 17:16:30 +0800

    Polish examples

--- Demo/DirectionFilterBank.py
+++ Demo/DirectionFilterBank.py
@@ -3,6 +3,7 @@ from imageio import imread
 import matplotlib.pyplot as plt
 from numpy.fft import fftshift, fft2, ifft2, ifftshift
 
+from Utility import display_subbands
 
 ''' Testing'''
 if __name__ == '__main__':
@@ -25,31 +26,14 @@ if __name__ == '__main__':
     P_0 = DirectionalFilterBankDown()
     U_0 = DirectionalFilterBankUp(P_0)
 
-    outnode = P_0
-    out = outnode.run(s_fftim)
+    outnode = U_0
+    out = P_0.run(s_fftim)
+    recovered = outnode.run(s_fftim)
 
-
-    '''Display image'''
-    # plt.imshow((np.abs(out)), vmin=0, vmax=3500)
-    # plt.imshow(ifft2(fftshift(out)).real - ifftshift(im))
-    # plt.show()
-
-    '''Display subband components'''
-    M = out.shape[-1]
-    ncol=4
-    fig, axs = plt.subplots(M / ncol, ncol)
-    for i in xrange(M):
-        if M <= ncol:
-            try:
-                axs[i].imshow(ifft2(fftshift(out[:,:,i])).real, cmap='Greys_r')
-                # axs[i].imshow(np.abs(fftshift(out[:,:,i])), vmin=0, vmax=2500)
-                axs[i].set_title('%s'%i)
-            except:
-                pass
-        else:
-            axs[i//ncol, i%ncol].imshow(ifft2(fftshift(out[:,:,i])).real, cmap='Greys_r')
-            # axs[i//ncol, i%ncol].imshow(np.abs(fftshift(out[:,:,i])), vmin=0, vmax=2500)
-            axs[i//ncol, i%ncol].set_title('%s'%i)
+    '''Display recovered image'''
+    plt.imshow(ifft2(fftshift(recovered)).real - ifftshift(im))
     plt.show()
 
+    '''Display subband components'''
+    display_subbands(out, ncol=4)
 
